246a328060011dd40da02144f536a6901d1b4bd9
Kusuma-P29 <92969202+Kusuma-P29@users.noreply.github.com>
2021-11-15 10:57:39 +0530

    Removed logs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		log.info("This is feature branch");
 	}
 
 }
